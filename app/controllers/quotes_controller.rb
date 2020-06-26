61eacbb247509bbbdf25d9b1009195b08132b974
amyhenning <henning.amy@gmail.com>
2020-06-26 15:55:03 -0500

    Some error handling experiments

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,4 +1,8 @@
 class QuotesController < Rulers::Controller
+  def exception
+    raise "It's a bad one!"
+  end
+
   def a_quote
     "There is nothing either good or bad" + 
       "but thinking makes it so."
